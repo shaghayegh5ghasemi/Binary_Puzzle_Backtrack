d667274569569b752c4f08df0538d1238dab68b7
Shaghayegh Ghasemi <shaghayegh5ghasemi@gmail.com>
2021-07-19 11:04:54 +0430

    project finished

--- backtrack.py
+++ backtrack.py
@@ -33,6 +33,7 @@ class PUZZLE:
                     return False
         return True
 
+    #ckeck if there is any conflict in conditions by assigning new value to a cell
     def isConsistent(self, i, j, value):
         temptable = copy.deepcopy(self.table)
         temptable[i][j].digit = value
@@ -65,12 +66,17 @@ class PUZZLE:
                 return False
         return True
 
-    def select_variable(self):
+    #return the index of an unassigned cell using mrv
+    def select_variable(self, domains):
+        temp = []
         for i, rows in enumerate(self.table):
             for j, cell in enumerate(rows):
                 if cell.digit == '-':
-                    return (i, j)
+                    temp.append(((i, j), len(domains[(i, j)])))
+        temp.sort(key=lambda x:x[1])
+        return temp[0][0]
 
+    # check if all unassigned cells have values to take
     def check_consistency(self, new_domain):
         for i in range(self.size):
             for j in range(self.size):
@@ -78,16 +84,10 @@ class PUZZLE:
                     return False
         return True
 
-    def update_domain(self, i, j, value):
-        pass
-
-    def least_constraint_value(self, puzzle, i, j):
-        pass
-
 #csp model to solve the problem
 class CSP:
-
-    def forward_checking(self, i, j, value, problem, domains):
+    #after assigning one value to a cell, we update domains of unassigned cells to delete the digits that will conflicts in some conditions
+    def forward_checking(self, i, j, problem, domains):
         new_domain = copy.deepcopy(domains)
         new_domain[(i, j)] = []
         for m in range(problem.size):
@@ -98,18 +98,46 @@ class CSP:
                             new_domain[(m, n)].remove(val)
         return new_domain
 
-    def mac(self):
-        pass
+    #check domain for arc consistency using AC3
+    def mac(self, i, j, problem, domains):
+        temp_problem = copy.deepcopy(problem)
+        new_domain = copy.deepcopy(domains)
+        new_domain[(i, j)] = []
+        queue = []
+        for m in range(problem.size):
+            for n in range(problem.size):
+                if problem.table[m][n].assigned == False:
+                    for val in new_domain[(m, n)]:
+                        if problem.isConsistent(m, n, val) == False:
+                            new_domain[(m, n)].remove(val)
+                            queue.append((m, n)) #if domain changes add neighbour to the queue
+        for node in queue:
+            if len(new_domain[node]) == 0:
+                continue
+            else:
+                temp_problem.table[node[0]][node[1]].digit = new_domain[node][0]
+                temp_problem.table[node[0]][node[1]].assigned = True
+                for k in range(temp_problem.size):
+                    for l in range(temp_problem.size):
+                        if temp_problem.table[k][l].assigned == False:
+                            for val in new_domain[(k, l)]:
+                                if temp_problem.isConsistent(k, l, val) == False:
+                                    new_domain[(k, l)].remove(val)
+                                    queue.append((k, l)) #if domain changes add neighbour to the queue
+        return new_domain
 
+
+    # backtrack algorithm to solve the problem
     def backtrack(self, problem, domains):
         if (problem.isComplete()):
             return problem
-        i, j = problem.select_variable()
+        i, j = problem.select_variable(domains)
         for value in domains[(i, j)]:
             if problem.isConsistent(i, j, value):
                 problem.table[i][j].digit = value
                 problem.table[i][j].assigned = True
-                new_domain = self.forward_checking(i, j, value, problem, domains)
+                new_domain = self.forward_checking(i, j, problem, domains)
+                #new_domain = self.mac(i, j, problem, new_domain)
                 if problem.check_consistency(new_domain):
                     result = self.backtrack(problem, new_domain)
                     if result:
@@ -144,7 +172,7 @@ if __name__ == '__main__':
     csp_model = CSP()
     result = csp_model.backtrack(puzzle, domains)
     print("The answer is:")
-    if (result):
+    if result:
         puzzle.print()
     else:
         print("Solving the puzzle was not successful!")
